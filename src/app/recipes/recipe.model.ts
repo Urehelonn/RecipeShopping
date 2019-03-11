92012efb3e116be758bf74a9fc090febb414cc62
Urehelonn <urehelonn@gmail.com>
2019-03-11 03:26:36 +0000

    recipe list filled, recipe detail filled.

---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,12 @@
+export class Recipe{
+    
+    public name: string;
+    public description: string;
+    public img: string;
+    
+    constructor(name: string, description: string, img: string){
+        this.name = name;
+        this.description = description;
+        this.img = img;
+    }
+}
